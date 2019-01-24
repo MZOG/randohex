3f613f648b1f312f35e72129a30cadd0895dabde
Marcin <hello@zogdev.co.uk>
2019-01-24 01:50:40 +0000

    create basic layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="randomColor"></div>
+
+    <div class="colorInfo">
+      <p>Color info:</p>
+      <p>Hex: <span class="colorHex"></span></p>
+      <p>RGB: <span class="colorRgb"></span></p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  methods: {
+    getRandomColor: function() {
+      
+      let letters = '0123456789ABCDEF';
+      let color = '#';
+
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+
+      // get randomColor div and change background
+      let colorContainer = document.querySelector('.randomColor');
+      colorContainer.style.backgroundColor = color;
+
+      // get colorHex paragraph and write the hex code into page
+      let colorHex = document.querySelector('.colorHex');
+      colorHex.innerHTML = color;
+
+      return color;
+    },
+
+    convertHexToRGB: function(color) {
+      
+      let hex = color.replace('#','');
+      let r = parseInt(hex.substring(0,2), 16);
+      let g = parseInt(hex.substring(2,4), 16);
+      let b = parseInt(hex.substring(4,6), 16);
+
+      // get colorRgb paragraph and write the rgb code into page
+      let colorRgb = document.querySelector('.colorRgb');
+      colorRgb.innerHTML = '('+ r + ', ' + g + ', ' + b + ')';
+      
+    }
+  },
+  mounted: function() {
+    this.convertHexToRGB(this.getRandomColor());
   }
 }
 </script>
